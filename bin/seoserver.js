b5e5b348e25038a13171fa6f57618a9a7f72d773
Richard Flosi <richard.flosi@gmail.com>
2013-08-20 15:51:37 -0700

    adding logging for debugging; cleaning up js per jshint

--- bin/seoserver.js
+++ bin/seoserver.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
 
-
-
-var program = require('commander');
-var fs = require('fs');
-var forever = require('forever-monitor');
+var program = require('commander'),
+    fs = require('fs'),
+    forever = require('forever-monitor');
 
 // require our seoserver npm package
 
 program
-  .version('0.0.1')
-  .option('-p, --port <location>', 'Specifiy a port to run on')
-
+    .version('0.0.1')
+    .option('-p, --port <location>', 'Specifiy a port to run on');
 
 program
-  .command('start')
-  .description('Starts up an SeoServer on default port 3000')
-  .action(function () {
-    var child = new (forever.Monitor)(__dirname + '/../lib/seoserver.js', {
-      options: [program.port]
+    .command('start')
+    .description('Starts up an SeoServer on default port 3000')
+    .action(function() {
+        var child = new (forever.Monitor)(__dirname + '/../lib/seoserver.js', {
+            options: [program.port]
+        });
+        child.start();
+        console.log(__dirname, 'SeoServer successfully started');
     });
-    child.start();
-    console.log(__dirname, 'SeoServer successfully started');
-  });
+
+program
+    .command('stop')
+    .description('Stops the SeoServer')
+    .action(function() {
+        console.log(__dirname, 'SeoServer stop stub. What should this do?');
+    });
 
 program.parse(process.argv);
-
